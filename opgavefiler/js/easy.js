b859aeb57ac2c10d6ee43ddb6efdee88cbfbc80d
hckjensen <hckjensen96@gmail.com>
2023-09-28 08:30:11 +0200

    Update easy.js

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,7 +1,9 @@
 /* opgave 1 */
 console.group("opgave 1");
 // din kode her
-
+for (let i = 0; i <= 10; i++ ){
+    console.log(i);
+}
 
 console.groupEnd();
 
@@ -9,7 +11,9 @@ console.groupEnd();
 /* opgave 2 */
 console.group("opgave 2");
 // din kode her
-
+for(let i = 10; i <= 11; i++){
+    console.log(i);
+}
 
 console.groupEnd();
 
@@ -17,7 +21,9 @@ console.groupEnd();
 /* opgave 3 */
 console.group("opgave 3");
 // din kode her
-
+for(let i = 2; i <= 40; i += 2){
+    console.log(i);
+}
 
 console.groupEnd();
 
